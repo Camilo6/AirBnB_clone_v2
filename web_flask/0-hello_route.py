243cccdbbb7d2439c76940a7eca4caaf15d52d58
Camilo <trresqwe@gmail.com>
2021-01-19 15:06:38 -0500

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" """
+""" Hello Flask! """
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def hello_world():
+    """main"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
