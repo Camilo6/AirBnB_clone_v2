0a8f936f2c06cbb352766a3c52982a0369dcc8a6
Camilo <trresqwe@gmail.com>
2021-01-20 11:16:20 -0500

    Update files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,3 +84,7 @@ class DBStorage():
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session)
         self.__session = Session()
+
+    def close(self):
+        """Close SQLAlchemy """
+        self.__session.close()
