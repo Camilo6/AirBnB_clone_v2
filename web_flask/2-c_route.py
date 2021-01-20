4f570e5b659fdc09b056427eae4473f5c7db619d
Camilo <trresqwe@gmail.com>
2021-01-20 09:19:56 -0500

    Update 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" HBNB """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello():
+    """return hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", methods=["GET"], strict_slashes=False)
+def hbn():
+    """return HBNB"""
+    return 'HBNB'
+
+
+@app.route("/c/<string:argument>", methods=["GET"], strict_slashes=False)
+def cisfun(argument):
+    """return C ..."""
+    return 'C {}'.format(argument.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
