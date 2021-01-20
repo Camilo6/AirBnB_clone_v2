cb6f8ac4b76509a4ff667d9c8f6ef501cdfe7e4a
Camilo <trresqwe@gmail.com>
2021-01-20 17:47:30 -0500

    Update 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+""" List of states """
+
+
 from models import storage
 from flask import Flask
 from flask import render_template
-
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
+    """Dcities by states"""
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Remove SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+    
